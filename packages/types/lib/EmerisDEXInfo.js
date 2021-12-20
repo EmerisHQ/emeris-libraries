46d392890de006313421b6979373f945d798e0e1
Clockwork <alex@clockwork.gr>
2021-12-20 13:58:22 +0200

    fix: use strict exports

--- packages/types/lib/EmerisDEXInfo.js
+++ packages/types/lib/EmerisDEXInfo.js
@@ -2,13 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SwapsResponse = exports.Swaps = exports.Swap = exports.SwapType = exports.DEX = exports.Denom = void 0;
 const typebox_1 = require("@sinclair/typebox");
-exports.Denom = typebox_1.Type.Object({
+exports.Denom = typebox_1.Type.Strict(typebox_1.Type.Object({
     name: typebox_1.Type.String(),
     displayName: typebox_1.Type.String(),
     denom: typebox_1.Type.String(),
     baseDenom: typebox_1.Type.String(),
     precision: typebox_1.Type.Number()
-});
+}));
 var DEX;
 (function (DEX) {
     DEX["Gravity"] = "gravity";
@@ -20,7 +20,7 @@ var SwapType;
     SwapType["Pool"] = "pool";
     SwapType["Swap"] = "swap";
 })(SwapType = exports.SwapType || (exports.SwapType = {}));
-exports.Swap = typebox_1.Type.Object({
+exports.Swap = typebox_1.Type.Strict(typebox_1.Type.Object({
     name: typebox_1.Type.String(),
     id: typebox_1.Type.String(),
     chainId: typebox_1.Type.String(),
@@ -29,8 +29,8 @@ exports.Swap = typebox_1.Type.Object({
     denomB: exports.Denom,
     swapPrice: typebox_1.Type.String(),
     swapType: typebox_1.Type.Enum(SwapType)
-});
+}));
 exports.Swaps = typebox_1.Type.Array(exports.Swap);
-exports.SwapsResponse = typebox_1.Type.Object({
+exports.SwapsResponse = typebox_1.Type.Strict(typebox_1.Type.Object({
     swaps: exports.Swaps
-});
+}));
--- packages/types/src/EmerisDEXInfo.ts
+++ packages/types/src/EmerisDEXInfo.ts
@@ -1,11 +1,11 @@
 import { Static, Type } from "@sinclair/typebox";
-export  const Denom = Type.Object({
+export  const Denom = Type.Strict(Type.Object({
 	name: Type.String(),
 	displayName: Type.String(),
 	denom: Type.String(),
 	baseDenom: Type.String(),
 	precision: Type.Number()
-});
+}));
 export enum DEX {
 	Gravity = 'gravity',
 	Osmosis = 'osmosis',
@@ -15,7 +15,7 @@ export enum SwapType {
 	Pool = 'pool',
 	Swap = 'swap'
 }
-export  const Swap = Type.Object({
+export  const Swap = Type.Strict(Type.Object({
 	name: Type.String(),
 	id: Type.String(),
 	chainId: Type.String(),
@@ -24,12 +24,12 @@ export  const Swap = Type.Object({
 	denomB: Denom,
 	swapPrice: Type.String(),
 	swapType: Type.Enum(SwapType)
-});
+}));
 export  const Swaps = Type.Array(Swap);
 
-export const SwapsResponse = Type.Object({
+export const SwapsResponse = Type.Strict(Type.Object({
   swaps: Swaps
-});
+}));
 export type Denom = Static<typeof Denom>;
 export type Swap = Static<typeof Swap>;
 export type Swaps = Static<typeof Swaps>;
