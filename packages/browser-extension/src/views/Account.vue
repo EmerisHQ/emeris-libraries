de180946f5f7d02288e8bb2b133ca95c93f964e8
Fabian Weber <fabian.weber@tendermint.com>
2021-12-22 15:00:27 +0400

    added account back button

--- packages/browser-extension/src/views/Account.vue
+++ packages/browser-extension/src/views/Account.vue
@@ -5,6 +5,7 @@
         <!-- TODO add icon to Demeris -->
         <img :src="require('@@/assets/CoqIcon.svg')" @click="$router.push('/settings')" />
       </template>
+      <Icon name="ChevronRightIcon" @click="$router.go(-1)" />
     </Header>
     <img
       style="height: 72px; width: 72px; margin-top: 40px; margin-left: auto; margin-right: auto"
