f948fb24634e3b635818e729ec0941adeaf62273
Fabian Weber <fabian.weber@tendermint.com>
2022-01-15 12:38:20 +0530

    go to portfolio after account confirm

--- packages/browser-extension/src/views/AccountReady.vue
+++ packages/browser-extension/src/views/AccountReady.vue
@@ -6,7 +6,7 @@
     <div class="secondary-text" style="text-align: center; margin-bottom: 24px">
       You have successfully created and backed up your new wallet. Enjoy exploring the new era of DeFi with Emeris.
     </div>
-    <router-link to="/">
+    <router-link to="/portfolio">
       <Button name="Go to portfolio" />
     </router-link>
   </div>
