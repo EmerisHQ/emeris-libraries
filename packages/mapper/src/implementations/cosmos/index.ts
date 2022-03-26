c6502269481840fa886de6918529ac96ccf29a62
Fabian Weber <fabian.weber@tendermint.com>
2022-03-26 18:43:10 +0700

    add type conversion to mapper

--- packages/mapper/src/implementations/cosmos/index.ts
+++ packages/mapper/src/implementations/cosmos/index.ts
@@ -1,15 +1,19 @@
-import { EmerisTransactions} from "@emeris/types";
+import { EmerisTransactions } from "@emeris/types";
 import { EmerisMessageMapper } from "../../EmerisMessageMapper";
 import * as Long from "long";
 
 export default class CosmosAminoMessageMapper extends EmerisMessageMapper {
-	
-    transfer(transaction: EmerisTransactions.AbstractTransferTransactionData, signing_address: string) {        
+
+    transfer(transaction: EmerisTransactions.AbstractTransferTransactionData, signing_address: string) {
         return {
             type: "cosmos-sdk/MsgSend",
             value: {
-                amount: [transaction.amount],
-                to_address: transaction.toAddress,
+                amount: [{
+                    amount: String(transaction.amount.amount),
+                    denom: transaction.amount.denom
+                }],
+                // @ts-ignore
+                to_address: transaction.to_address,
                 from_address: signing_address,
             },
         };
@@ -22,7 +26,8 @@ export default class CosmosAminoMessageMapper extends EmerisMessageMapper {
                 source_port: 'transfer',
                 source_channel: transaction.through,
                 sender: signing_address,
-                receiver: transaction.toAddress,
+                // @ts-ignore
+                receiver: transaction.to_address,
                 timeout_timestamp: Long.fromString(new Date().getTime() + 300000 + '000000'),
                 //timeoutHeight: { revisionHeight: "10000000000",revisionNumber:"0"},
                 token: { ...transaction.amount },
