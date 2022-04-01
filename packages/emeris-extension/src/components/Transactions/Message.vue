36dac8191bae1e4609c3d20c45a9ce60ee0f2de1
Clockwork <alex@clockwork.gr>
2022-04-01 12:01:25 +0300

    fix: replace message dynamic import

--- packages/emeris-extension/src/components/Transactions/Message.vue
+++ packages/emeris-extension/src/components/Transactions/Message.vue
@@ -4,23 +4,44 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { EmerisTransactions } from '@emeris/types';
 import { defineAsyncComponent } from '@vue/runtime-core';
 import Fallback from './fallback';
+import addliquidity from './addliquidity.vue';
+import custom from './custom.vue';
+import ibcTransfer from './ibcTransfer.vue';
+import swap from './swap.vue';
+import transfer from './transfer.vue';
+import withdrawliquidity from './withdrawliquidity.vue';
 
-// dynamically loads messages by message type
-// files nmaes in ./ need have the same name as the message type
 export default {
   name: 'Message',
   props: {
-    message: { type: EmerisTransactions.TransactionData, required: true },
+    message: { type: Object as PropType<EmerisTransactions.AbstractTransaction>, required: true },
     chainId: { type: String, required: true },
   },
   computed: {
     component() {
       return defineAsyncComponent({
-        loader: () => Fallback,
+        loader: () => {
+          switch ((this.message as EmerisTransactions.AbstractTransaction).type) {
+            case 'addLiquidity':
+              return addliquidity;
+            case 'withdrawLiquidity':
+              return withdrawliquidity;
+            case 'transfer':
+              return transfer;
+            case 'swap':
+              return swap;
+            case 'IBCtransfer':
+              return ibcTransfer;
+            case 'createPool':
+              return addliquidity;
+            default:
+              return custom;
+          }
+        },
         errorComponent: Fallback,
       });
     },
